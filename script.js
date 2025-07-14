5ad5f254fc782f6862634413373c94f1176be0ce
flywithmubashir7 <muradkatto@gmail.com>
2025-07-14 18:57:10 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Team Dangerous Script - Starter Version
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Team Dangerous site loaded! Get ready for action. 🛡️🔥");
+
+  // You can add dynamic features here later
+});
